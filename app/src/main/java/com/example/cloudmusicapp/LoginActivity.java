d430b79d40bebd91e477812b9d5440085c5d8e38
Sichongzou <1557157806@qq.com>
2021-01-05 21:00:36 +0800

    Sichongzou

--- app/src/main/java/com/example/cloudmusicapp/LoginActivity.java
+++ app/src/main/java/com/example/cloudmusicapp/LoginActivity.java
@@ -2,13 +2,31 @@ package com.example.cloudmusicapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
-
+    EditText userName,password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        userName=findViewById(R.id.editText_userName);
+        password=findViewById(R.id.editText_password);
+    }
+    public void login(View view){
+        if(userName.getText().toString().equals("Sichongzou")){
+            if(password.getText().toString().equals("123456")){
+                Intent intent=new Intent();
+                intent.setClass(this,MainActivity.class);
+                startActivity(intent);
+                Toast.makeText(this,"登陆成功!",Toast.LENGTH_SHORT).show();
+                return;
+            }
+        }
+        Toast.makeText(this,"用户名或密码错误，请重试。",Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/example/cloudmusicapp/MeActivity.java
@@ -0,0 +1,14 @@
+package com.example.cloudmusicapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_me);
+    }
+}
